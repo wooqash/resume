ae0709e77d8db988e0430817b5ec999eebd1afce
wooqash <wooqash.webdesign@gmail.com>
2022-04-12 23:33:20 +0200

    chore: disabled locale detection

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     defaultLocale: 'pl',
-
+    localeDetection: false,
   },
 }
 

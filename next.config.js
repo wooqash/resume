5e7672cda4dc28be7c2d119ca67d49896c73d2e6
wooqash <wooqash.webdesign@gmail.com>
2022-04-12 23:09:42 +0200

    feat: nextjs project with typescript initialization

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['pl', 'en-GB'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'pl',
+
+  },
+}
+
+module.exports = nextConfig

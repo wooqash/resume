1ff561167c40ede16aa30e5c2a84e33bf6fb3480
wooqash <wooqash.webdesign@gmail.com>
2022-04-15 16:14:57 +0200

    chore(tsconfig): extended basic config by couple useful rules
    added rules: baseUrl, incremental, removeComments, importHelpers, paths

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   i18n: {
     // These are all the locales you want to support in
     // your application
-    locales: ['pl', 'en-GB'],
+    locales: ['pl', 'en'],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     defaultLocale: 'pl',

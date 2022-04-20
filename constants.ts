ff4839e594e1634d1f9d94ef2fa1c3f7fb9a7877
wooqash <wooqash.webdesign@gmail.com>
2022-04-20 14:48:29 +0200

    feat(constant): added some default translations and translations to about & contact

--- constants.ts
+++ constants.ts
@@ -1,12 +1,107 @@
+import { ISeo } from "types/seo";
+
 interface Translations {
-    [localization: string] : { [key: string] : string }
+  [localization: string]: { [key: string]: ISeo } | { [key: string]: string };
 }
-  
+
+export const DEFAULT_LABELS: Translations = {
+  pl: {
+    ariaNewTabLabel: "Otwórz w nowej karcie",
+  },
+  en: {
+    ariaNewTabLabel: "Open in new tab",
+  },
+};
+
 export const HOME: Translations = {
-    pl : {
-        title: "Hello world - PL"
+  pl: {
+    seo: {
+      metaTitle: "Hello world - PL",
+      metaTitleSuffix: "Łukasz Sobola",
+      metaDescription: "",
+      metaKeywords: "",
+      preventIndexing: false,
+      preventFollowing: false,
+      cannonicalLink: "",
+      // shareImage: '',
+      // openGraph: '',
+      // twitterCard: '',
     },
-    en : {
-        title: "Hello world - EN"
-    }
-}
+  },
+  en: {
+    seo: {
+      metaTitle: "Hello world - EN",
+      metaTitleSuffix: "Łukasz Sobola",
+      metaDescription: "",
+      metaKeywords: "",
+      preventIndexing: false,
+      preventFollowing: false,
+      cannonicalLink: "",
+      // shareImage: '',
+      // openGraph: '',
+      // twitterCard: '',
+    },
+  },
+};
+
+export const ABOUT: Translations = {
+  pl: {
+    seo: {
+      metaTitle: "O mnie",
+      metaTitleSuffix: "Łukasz Sobola",
+      metaDescription: "",
+      metaKeywords: "",
+      preventIndexing: false,
+      preventFollowing: false,
+      cannonicalLink: "",
+      // shareImage: '',
+      // openGraph: '',
+      // twitterCard: '',
+    },
+  },
+  en: {
+    seo: {
+      metaTitle: "About - EN",
+      metaTitleSuffix: "Łukasz Sobola",
+      metaDescription: "",
+      metaKeywords: "",
+      preventIndexing: false,
+      preventFollowing: false,
+      cannonicalLink: "",
+      // shareImage: '',
+      // openGraph: '',
+      // twitterCard: '',
+    },
+  },
+};
+
+export const CONTACT: Translations = {
+  pl: {
+    seo: {
+      metaTitle: "Kontakt",
+      metaTitleSuffix: "Łukasz Sobola",
+      metaDescription: "",
+      metaKeywords: "",
+      preventIndexing: false,
+      preventFollowing: false,
+      cannonicalLink: "",
+      // shareImage: '',
+      // openGraph: '',
+      // twitterCard: '',
+    },
+  },
+  en: {
+    seo: {
+      metaTitle: "Contact",
+      metaTitleSuffix: "Łukasz Sobola",
+      metaDescription: "",
+      metaKeywords: "",
+      preventIndexing: false,
+      preventFollowing: false,
+      cannonicalLink: "",
+      // shareImage: '',
+      // openGraph: '',
+      // twitterCard: '',
+    },
+  },
+};

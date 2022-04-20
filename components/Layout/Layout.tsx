47cafbb2b480a93d8a87bfcfba3dd473721273a3
wooqash <wooqash.webdesign@gmail.com>
2022-04-20 14:42:32 +0200

    feat(layout): added ModeSwitcher and LangSwitcher to the Layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,19 +1,39 @@
-import { ReactChild } from "react";
+import { MouseEvent, KeyboardEvent, ReactChild } from "react";
 import Navbar from "@/components/Navbar";
+import ModeSwitcher from "@/components/ModeSwitcher";
+import LangSwitcher from "@/components/LangSwitcher";
+import { useThemeContext } from "context/theme.context";
+import { useLangContext } from "context/lang.context";
+import style from "./Layout.module.scss";
 
 type LayoutProps = {
-  children: ReactChild
-}
- 
-const Layout:React.FC<LayoutProps> = ({children}) => {
+  children: ReactChild;
+};
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const { theme, changeTheme } = useThemeContext();
+  const { lang, changeLang } = useLangContext();
+
+  const handleThemeChange = (
+    e: KeyboardEvent<HTMLButtonElement> | MouseEvent<HTMLButtonElement>
+  ) => {
+    theme === "light" ? changeTheme("dark") : changeTheme("light");
+  };
+
+  const handleLangChange = (
+    e: KeyboardEvent<HTMLAnchorElement> | MouseEvent<HTMLAnchorElement>
+  ) => {
+    lang === "pl" ? changeLang("en") : changeLang("pl");
+  };
 
   return (
-        <>
-            <Navbar />
-            <main>{children}</main>
-        </>
+    <div className={`${style.container} ${style[theme]}`}>
+      <ModeSwitcher onModeChange={handleThemeChange} />
+      <LangSwitcher onLangChange={handleLangChange} />
+      <Navbar />
+      <main>{children}</main>
+    </div>
   );
-}
- 
- 
+};
+
 export default Layout;

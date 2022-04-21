e6bb17a3a8ff0e5e0031d8c09bd7229706de9b35
wooqash <wooqash.webdesign@gmail.com>
2022-04-21 23:33:15 +0200

    feat(types): created type for buttons

---
+++ types/button.ts
@@ -0,0 +1,11 @@
+export enum ButtonType {
+  PRIMARY = "primary",
+  SECONDARY = "secondary",
+}
+
+export interface IButton {
+  id: number | string;
+  label: string;
+  "aria-description"?: string;
+  type?: ButtonType;
+}

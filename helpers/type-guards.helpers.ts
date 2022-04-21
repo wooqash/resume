3b50d72471a9697ae8a75ca0be9f236077bd2362
wooqash <wooqash.webdesign@gmail.com>
2022-04-21 23:52:50 +0200

    feat(dependencies): added lib 'react-accessible-dropdown-menu-hook'

--- helpers/type-guards.helpers.ts
+++ helpers/type-guards.helpers.ts
@@ -1,10 +1,22 @@
+import { ButtonType, IButton } from "@/types/button";
 import { IMenuLink } from "@/types/menu-link";
 
 export const isIMenuLinkArr = (list: any): list is IMenuLink[] => {
   return (
+    list &&
     list.length > 0 &&
     typeof list[0].label === "string" &&
     typeof list[0].url === "string" &&
     typeof list[0].newTab === "boolean"
+  );
+};
+
+export const isIButton = (button: any): button is IButton => {
+  return (
+    typeof button.label === "string" &&
+    typeof button["aria-description"] === "string" &&
+    (button.type === ButtonType.PRIMARY ||
+      button.type === ButtonType.SECONDARY ||
+      button.type === undefined)
   );
 };

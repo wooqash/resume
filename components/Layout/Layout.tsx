00b0d5f4e7c7fe672f55dfcb4074ce3e117a579a
wooqash <wooqash.webdesign@gmail.com>
2022-04-23 07:39:12 +0200

    feat(components): created Layout component

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,21 @@
+import { ReactChild } from "react";
+import LangSwitcher from "../LangSwitcher";
+import ModeSwitcher from "../ModeSwitcher";
+import Navbar from "../Navbar";
+
+type LayoutProps = {
+  children: ReactChild;
+};
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <LangSwitcher />
+      <ModeSwitcher />
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;

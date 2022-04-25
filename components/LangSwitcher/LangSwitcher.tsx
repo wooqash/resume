3b5cc6b21d89526d29d883997f79b9dbcb46d761
wooqash <wooqash.webdesign@gmail.com>
2022-04-25 22:19:05 +0200

    feat(lang-switcher): added code for handling language changing

--- components/LangSwitcher/LangSwitcher.tsx
+++ components/LangSwitcher/LangSwitcher.tsx
@@ -1,7 +1,40 @@
-type LangSwitcherProps = {};
+import { Link, VisuallyHidden } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { KeyboardEvent, MouseEvent } from "react";
+import { useLanguageContext } from "contexts/language";
+import { GLOBAL_LABELS } from "translations.constants";
+import { isILink } from "helpers/type-guards.helpers";
 
-const LangSwitcher: React.FC<LangSwitcherProps> = () => {
-  return <div>LangSwitcher</div>;
+type LangSwitcherProps = {
+  onLangChange: (
+    e: KeyboardEvent<HTMLAnchorElement> | MouseEvent<HTMLAnchorElement>
+  ) => void;
+};
+
+const LangSwitcher: React.FC<LangSwitcherProps> = ({ onLangChange }) => {
+  const { lang } = useLanguageContext();
+  const gAriaNewTabLabel = typeof GLOBAL_LABELS[lang].ariaNewTabLabel;
+  const gLangSwitcher = GLOBAL_LABELS[lang].langSwitcher;
+  const switchLink = isILink(gLangSwitcher) ? gLangSwitcher : null;
+  const ariaNewTabLabel =
+    typeof gAriaNewTabLabel === "string" ? gAriaNewTabLabel : "";
+
+  return (
+    <>
+      {switchLink && (
+        <NextLink href={switchLink.url} passHref locale={lang}>
+          <Link>
+            <>
+              {switchLink.label}
+              {ariaNewTabLabel && (
+                <VisuallyHidden>{ariaNewTabLabel}</VisuallyHidden>
+              )}
+            </>
+          </Link>
+        </NextLink>
+      )}
+    </>
+  );
 };
 
 export default LangSwitcher;

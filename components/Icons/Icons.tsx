ee2ef4726aeb7dd2d58acc6087b6c02e5478c299
wooqash <wooqash.webdesign@gmail.com>
2022-04-25 13:22:47 +0200

    feat(icon): created Icon component and type

---
+++ components/Icons/Icons.tsx
@@ -0,0 +1,45 @@
+import { Icon } from "@chakra-ui/react";
+import { IconType } from "../../types/icon.interface";
+import {
+  FiHome,
+  FiUser,
+  FiBriefcase,
+  FiPhone,
+  FiShare2,
+  FiFacebook,
+  FiInstagram,
+  FiTwitter,
+  FiLinkedin,
+} from "react-icons/fi";
+
+type IconsProps = {
+  type: IconType;
+};
+
+const Icons: React.FC<IconsProps> = ({ type }) => {
+  switch (type) {
+    case IconType.HOME:
+      return <Icon as={FiHome} />;
+    case IconType.ABOUT:
+      return <Icon as={FiUser} />;
+    case IconType.PORTFOLIO:
+      return <Icon as={FiBriefcase} />;
+    case IconType.CONTACT:
+      return <Icon as={FiPhone} />;
+    case IconType.SOCIAL:
+      return <Icon as={FiShare2} />;
+    case IconType.FACEBOOK:
+      return <Icon as={FiFacebook} />;
+    case IconType.INSTAGRAM:
+      return <Icon as={FiInstagram} />;
+    case IconType.TWITTER:
+      return <Icon as={FiTwitter} />;
+    case IconType.LINKEDIN:
+      return <Icon as={FiLinkedin} />;
+    default:
+      let x: never = type;
+      return x;
+  }
+};
+
+export default Icons;

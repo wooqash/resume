720ad83a6fc8cd32c4f3952f21a0b675ff6f34e8
wooqash <wooqash.webdesign@gmail.com>
2022-04-25 21:24:20 +0200

    feat(language): added context and context provider for handling languages

---
+++ contexts/language.tsx
@@ -0,0 +1,24 @@
+import { useRouter } from "next/router";
+import { createContext, useContext, useState, ReactChild } from "react";
+import { LanguageContextType, Language } from "@/types/language.type";
+
+export const LanguageContext = createContext<LanguageContextType | null>(null);
+LanguageContext.displayName = "Language";
+
+const LanguageProvider: React.FC<{ children: ReactChild }> = ({ children }) => {
+  const router = useRouter();
+  const locale = (router.locale as Language) || "pl";
+  const [lang, setLang] = useState<Language>(locale);
+
+  return (
+    <LanguageContext.Provider value={{ lang: lang, changeLang: setLang }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguageContext = () => {
+  return useContext(LanguageContext) as LanguageContextType;
+};
+
+export default LanguageProvider;

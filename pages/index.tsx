69bd72b22e3587be3dedd840798954a67ed6cab3
wooqash <wooqash.webdesign@gmail.com>
2022-04-25 22:48:58 +0200

    feat(homepage): changed seo object

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,23 @@
 import Layout from "@/components/Layout";
 import Seo from "@/components/Seo";
+import { useLanguageContext } from "contexts/language";
+import { isISeo } from "helpers/type-guards.helpers";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { HOME_LABELS } from "translations.constants";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  // console.log('lint-staged test3');
-  const router = useRouter();
-
-  const seoMeta = {
-    title: "Resume",
-    titleSuffix: "Łukasz Sobola",
-    description: "Witam",
-    preventIndexing: false,
-    preventFollowing: false,
-  };
+  const { lang } = useLanguageContext();
+  const gSeoMeta = HOME_LABELS[lang].seo;
+  const seoMeta = isISeo(gSeoMeta) ? gSeoMeta : null;
 
   return (
     <Layout>
       <>
-        <Seo meta={seoMeta} />
-        <h1>Homepage</h1>
+        {seoMeta && <Seo meta={seoMeta} />}
+        <h1>{lang === "pl" ? "Strona główna" : "Homepage"}</h1>
       </>
     </Layout>
   );

4866ba6e700e85d6d9e367c08d373a30cbac98a8
wooqash <wooqash.webdesign@gmail.com>
2022-04-28 05:08:55 +0200

    refactor(types): removed unneeded interfaces for menu link and social link

--- types/menu-link.interface.ts
+++
@@ -1,6 +0,0 @@
-import { IconType } from "./icon.enum";
-import { ILink } from "./link.interface";
-
-export interface IMenuLink extends ILink {
-  icon?: IconType;
-}
--- types/social-link.interface.ts
+++
@@ -1,6 +0,0 @@
-import { IconType } from "./icon.enum";
-import { ILink } from "./link.interface";
-
-export interface ISocialLink extends ILink {
-  icon?: IconType;
-}

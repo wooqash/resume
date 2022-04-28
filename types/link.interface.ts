c9d1eff6c664a3d1f8cad05344944a8772bfcbeb
wooqash <wooqash.webdesign@gmail.com>
2022-04-28 05:11:20 +0200

    feat(types): addded aria attributes to ILink interface

--- types/link.interface.ts
+++ types/link.interface.ts
@@ -1,6 +1,14 @@
+import { IconType } from "./icon.enum";
+
 export interface ILink {
   id: string | number;
   label: string;
+  "aria-tooltip"?: string;
+  "aria-description"?: string;
   url: string;
   newTab: boolean;
+}
+
+export interface ILinkWithIcon extends ILink {
+  icon?: IconType;
 }

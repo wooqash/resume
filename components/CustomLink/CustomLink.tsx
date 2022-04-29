09f2768a4f4f0597c319d6e91ec67c4c1ad89de0
wooqash <wooqash.webdesign@gmail.com>
2022-04-29 06:21:15 +0200

    feat(custom-link): adjusted custom link component to handling child and tooltip wrapper

--- components/CustomLink/CustomLink.tsx
+++ components/CustomLink/CustomLink.tsx
@@ -1,82 +1,64 @@
-import Link from "next/link";
+import NextLink from "next/link";
 import { KeyboardEvent, MouseEvent, forwardRef, ReactChild } from "react";
-import { ILink } from "@/types/link.interface";
-import { VisuallyHidden } from "@chakra-ui/react";
+import { ILink, ILinkWithIcon } from "@/types/link.interface";
+import LinkWithTooltip from "./LinkWithTooltip";
+import LinkChild from "./LinkChild";
 
 import style from "./links.module.scss";
 
 type CustomLinkProps = {
-  id?: string | number;
-  url: string;
-  newTab?: boolean;
-
-  // link: ILink;
+  link: ILink | ILinkWithIcon;
   // className?: string;
   locale?: string;
   children?: ReactChild;
-  role?: string;
-  tabIndex?: number;
   handleClick?: (e: MouseEvent<HTMLAnchorElement>) => void;
   handleKeyDown?: (e: KeyboardEvent<HTMLAnchorElement>) => void;
 };
 
-const CustomLink = forwardRef<HTMLAnchorElement, CustomLinkProps>(
-  (props, ref) => {
-    const {
-      id,
-      url,
-      newTab,
-      locale,
-      children,
-      // className,
-      role,
-      tabIndex,
-      handleClick,
-    } = props;
+const CustomLink: React.FC<CustomLinkProps> = (props) => {
+  const {
+    link,
+    locale,
+    children,
+    // className,
+    handleClick,
+  } = props;
+  const { url, "aria-tooltip": labelledby } = link;
 
-    const isInternalLink = url.startsWith("/");
-    // const classes = className ? { className } : {};
-    const target = newTab ? { target: "_blank" } : { target: "_self" };
-    const rel = !isInternalLink && newTab ? { rel: "noopener noreferrer" } : {};
-    const roleAttr = role ? { role } : {};
+  const isInternalLink = url.startsWith("/");
+  // const classes = className ? { className } : {};
 
-    // For internal links, use the Next.js Link component
-    if (isInternalLink) {
-      const idAttr = id ? { id: `Internal_${id?.toString()}` } : {};
-      return (
-        <Link href={url} locale={locale} passHref>
-          <a
-            {...idAttr}
-            // { ...classes }
-            {...roleAttr}
-            tabIndex={tabIndex}
-            ref={ref}
-            onClick={handleClick}
-          >
-            {children}
-          </a>
-        </Link>
-      );
-    }
-
+  // For internal links, use the Next.js Link component
+  if (isInternalLink) {
     return (
-      <a
-        id={id?.toString()}
-        href={url}
-        {...target}
-        {...rel}
-        // { ...classes }
-        {...roleAttr}
-        tabIndex={tabIndex}
-        ref={ref}
-        onClick={handleClick}
-      >
-        {children}
-      </a>
+      <>
+        {labelledby ? (
+          <LinkWithTooltip link={link}>
+            <NextLink href={url} locale={locale} passHref>
+              <LinkChild link={link}>{children}</LinkChild>
+            </NextLink>
+          </LinkWithTooltip>
+        ) : (
+          <NextLink href={url} locale={locale} passHref>
+            <LinkChild link={link}>{children}</LinkChild>
+          </NextLink>
+        )}
+      </>
     );
   }
-);
+
+  return (
+    <>
+      {labelledby ? (
+        <LinkWithTooltip link={link}>
+          <LinkChild link={link}>{children}</LinkChild>
+        </LinkWithTooltip>
+      ) : (
+        <LinkChild link={link}>{children}</LinkChild>
+      )}
+    </>
+  );
+};
+CustomLink.displayName = "CustomLink";
 
 export default CustomLink;
-
-CustomLink.displayName = "CustomLink";

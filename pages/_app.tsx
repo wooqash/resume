cf9d2ddbe3d7d7b695ec52163d3a00f010ec8e96
wooqash <wooqash.webdesign@gmail.com>
2022-05-09 16:09:33 +0200

    feat(app): addded DefaultSeo component to the app and Chakra theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,39 @@ import "../styles/index.scss";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import LanguageProvider, { LanguageContext } from "contexts/language";
+import { DefaultSeo } from "next-seo";
+
+import theme from "../theme/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <LanguageProvider>
-        <Component {...pageProps} />
+        <>
+          <DefaultSeo
+            additionalLinkTags={[
+              {
+                rel: "preconnect",
+                href: "https://fonts.googleapis.com",
+              },
+              {
+                rel: "preconnect",
+                href: "https://fonts.googleapis.com",
+                crossOrigin: "crossorigin",
+              },
+              {
+                rel: "preload",
+                as: "style",
+                href: "https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600&family=Poppins:wght@700&display=swap",
+              },
+              {
+                rel: "stylesheet",
+                href: "https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600&family=Poppins:wght@700&display=swap",
+              },
+            ]}
+          />
+          <Component {...pageProps} />
+        </>
       </LanguageProvider>
     </ChakraProvider>
   );

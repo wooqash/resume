05ada9bea38cde975db0f970f311eaf7ab667542
wooqash <wooqash.webdesign@gmail.com>
2022-05-09 15:43:38 +0200

    feat(components): implemented a ModeSwitcher component

--- components/ModeSwitcher/ModeSwitcher.tsx
+++ components/ModeSwitcher/ModeSwitcher.tsx
@@ -1,14 +1,43 @@
-import { Stack, Switch, useColorMode } from "@chakra-ui/react";
+import { Icon, HStack, Switch, useColorMode } from "@chakra-ui/react";
+import { FiMoon, FiSun } from "react-icons/fi";
 
 type ModeSwitcherProps = {};
 
 const ModeSwitcher: React.FC<ModeSwitcherProps> = () => {
-  const { toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <>
-      <Stack direction="row">
-        <Switch colorScheme="yellow" onChange={toggleColorMode} />
-      </Stack>
+      <HStack spacing="5px" pos="relative">
+        {colorMode == "light" && (
+          <Icon
+            as={FiMoon}
+            color="gray.200"
+            pos="absolute"
+            zIndex="2"
+            right="4px"
+            boxSize="10px"
+          />
+        )}
+        {colorMode == "dark" && (
+          <Icon
+            as={FiSun}
+            color="blackAlpha.900"
+            pos="absolute"
+            zIndex="2"
+            left="10px"
+            boxSize="10px"
+          />
+        )}
+        <Switch
+          colorScheme="yellow"
+          onChange={toggleColorMode}
+          pos="relative"
+          zIndex="1"
+          size="md"
+          defaultChecked={true}
+        />
+      </HStack>
     </>
   );
 };

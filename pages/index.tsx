190b8c0e6b623fb063a709464071aecd0a999b41
wooqash <wooqash.webdesign@gmail.com>
2022-05-09 16:10:41 +0200

    feat(pages): added structure to the Index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
+import Intro from "@/components/Intro";
 import Layout from "@/components/Layout";
+import IntroImage from "@/components/IntroImage";
 import Seo from "@/components/Seo";
+import { Box, Flex } from "@chakra-ui/react";
 import { useLanguageContext } from "contexts/language";
 import { isISeo } from "helpers/type-guards.helpers";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import { HOME_LABELS } from "translations.constants";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const { lang } = useLanguageContext();
@@ -17,7 +17,46 @@ const Home: NextPage = () => {
     <Layout>
       <>
         {seoMeta && <Seo meta={seoMeta} />}
-        <h1>{lang === "pl" ? "Strona główna" : "Homepage"}</h1>
+        {/* <h1>{lang === "pl" ? "Strona główna" : "Homepage"}</h1> */}
+        {/* <Container centerContent> */}
+        <Box
+          pos="fixed"
+          h="200%"
+          width="100%"
+          transform="rotate(15deg)"
+          left="-83%"
+          top="-50%"
+          backgroundColor="teal.600"
+          zIndex="0"
+          d={{ base: "none", lg: "block" }}
+        ></Box>
+        <Flex
+          minH="100vh"
+          w={{ lg: "100%" }}
+          m={["0 auto"]}
+          p={{ base: "4rem 2rem 6rem", lg: "8" }}
+          gap="6"
+          alignItems="center"
+          justifyContent="center"
+          flexDir={{ base: "column", lg: "row" }}
+          zIndex="1"
+          pos="relative"
+        >
+          <IntroImage />
+          <Intro />
+        </Flex>
+        {/* <Box
+          pos="fixed"
+          h="200%"
+          width="100%"
+          transform="rotate(15deg)"
+          right="-95%"
+          top="-50%"
+          backgroundColor="teal.600"
+          zIndex="0"
+          d={["none", null, null, "block"]}
+        ></Box> */}
+        {/* </Container> */}
       </>
     </Layout>
   );

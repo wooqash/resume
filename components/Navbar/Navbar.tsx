4518be3a2b53ffa3ff36699ab174b664ef7e4c6d
wooqash <wooqash.webdesign@gmail.com>
2022-05-10 06:07:27 +0200

    fix(components): changed the condition for sticky bottom nav for mobile devices

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -66,7 +66,7 @@ const Navbar: React.FC<NavbarProps> = () => {
     if (
       isMobile &&
       isScrollHeightIsNumber &&
-      window.innerHeight === scrollHeight
+      window.innerHeight >= scrollHeight
     ) {
       setStickyBottomNavAnimation(true);
       return;

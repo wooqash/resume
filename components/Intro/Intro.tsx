73b6671f7a23a96a6b36843e74061db9d2a8bdb0
wooqash <wooqash.webdesign@gmail.com>
2022-05-10 17:07:30 +0200

    feat(components): added additional validation of type for Intro component

--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -9,41 +9,43 @@ type IntroProps = {};
 const Intro: React.FC<IntroProps> = (props) => {
   const { lang } = useLanguageContext();
   const pageTrans = HOME_LABELS[lang].intro;
-  const intro = isIIntro(pageTrans) ? pageTrans : null;
+  const intro = pageTrans && isIIntro(pageTrans) ? pageTrans : null;
 
   return (
-    <Box
-      maxW={{ base: "550px", lg: "66.66666667%" }}
-      ml={{ lg: "33.33333333%" }}
-      zIndex="1"
-    >
-      <VStack
-        alignItems={{ base: "center", lg: "flex-start" }}
-        spacing="6"
-        maxW={{ base: "100%", lg: "60%" }}
-        m="0 auto"
-        textAlign={{ base: "center", lg: "left" }}
+    intro && (
+      <Box
+        maxW={{ base: "550px", lg: "66.66666667%" }}
+        ml={{ lg: "33.33333333%" }}
+        zIndex="1"
       >
-        {intro?.title && (
-          <Heading textTransform="uppercase">
-            {intro?.titlePrefix && (
-              <Box as="p" color="teal.600">
-                {intro?.titlePrefix}
-              </Box>
-            )}
-            {intro?.title}
-          </Heading>
-        )}
-        {intro?.introText && <Text>{intro?.introText}</Text>}
-        {intro?.moreBtn && intro?.moreBtn.href && (
-          <NextLink href={intro?.moreBtn.href} locale={lang} passHref>
-            <Button as="a" rightIcon={<Icon as={FiArrowRight} />}>
-              {intro?.moreBtn.label}
-            </Button>
-          </NextLink>
-        )}
-      </VStack>
-    </Box>
+        <VStack
+          alignItems={{ base: "center", lg: "flex-start" }}
+          spacing="6"
+          maxW={{ base: "100%", lg: "60%" }}
+          m="0 auto"
+          textAlign={{ base: "center", lg: "left" }}
+        >
+          {intro?.title && (
+            <Heading textTransform="uppercase">
+              {intro?.titlePrefix && (
+                <Box as="p" color="teal.600">
+                  {intro?.titlePrefix}
+                </Box>
+              )}
+              {intro?.title}
+            </Heading>
+          )}
+          {intro?.introText && <Text>{intro?.introText}</Text>}
+          {intro?.moreBtn && intro?.moreBtn.href && (
+            <NextLink href={intro?.moreBtn.href} locale={lang} passHref>
+              <Button as="a" rightIcon={<Icon as={FiArrowRight} />}>
+                {intro?.moreBtn.label}
+              </Button>
+            </NextLink>
+          )}
+        </VStack>
+      </Box>
+    )
   );
 };
 

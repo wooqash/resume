e1ec23e0f0b7a65597b82ab62265136c91caf72c
wooqash <wooqash.webdesign@gmail.com>
2022-05-10 17:15:02 +0200

    feat(components): added basic logic and structure to the Title component

--- components/Title/Title.tsx
+++ components/Title/Title.tsx
@@ -1,7 +1,53 @@
+import { Box, Heading, VStack } from "@chakra-ui/react";
+import { useLanguageContext } from "contexts/language";
+import { isIHeading } from "helpers/type-guards.helpers";
+import { stringify } from "querystring";
+import { ABOUT_LABELS } from "translations.constants";
+
 type TitleProps = {};
 
 const Title: React.FC<TitleProps> = () => {
-  return <div>Title</div>;
+  const { lang } = useLanguageContext();
+  const pageTrans = ABOUT_LABELS[lang].heading;
+  const heading = pageTrans && isIHeading(pageTrans) ? pageTrans : null;
+
+  const colorLastWordOfTitle = (title: string) => {
+    const titleElem = title.split(" ");
+    if (titleElem.length > 1) {
+      const wordsWithoutColor = title.substring(0, title.lastIndexOf(" "));
+      const wordToColor = titleElem[titleElem.length - 1];
+      return (
+        <>
+          {wordsWithoutColor}{" "}
+          <Box as="span" color="teal.600">
+            {wordToColor}
+          </Box>
+        </>
+      );
+    }
+    return title;
+  };
+
+  return (
+    heading && (
+      <VStack pos="relative" textTransform="uppercase" alignItems="center">
+        <Heading
+          as="h1"
+          position="absolute"
+          top="40%"
+          fontSize="3.5rem"
+          textAlign="center"
+        >
+          {colorLastWordOfTitle(heading.title)}
+        </Heading>
+        {heading?.shadowTitle && (
+          <Heading as="span" opacity="0.07" fontSize="6.875rem">
+            {heading.shadowTitle}
+          </Heading>
+        )}
+      </VStack>
+    )
+  );
 };
 
 export default Title;

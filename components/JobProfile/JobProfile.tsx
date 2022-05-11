e57dc38202a693cdf441982916b58dc8469bfae4
wooqash <wooqash.webdesign@gmail.com>
2022-05-11 09:32:03 +0200

    feat(components): added translations keys to Job Profile component

--- components/JobProfile/JobProfile.tsx
+++ components/JobProfile/JobProfile.tsx
@@ -1,28 +1,30 @@
 import { Text, VStack, useColorMode } from "@chakra-ui/react";
+import { useLanguageContext } from "contexts/language";
+import { isIJobProfile } from "helpers/type-guards.helpers";
+import { ABOUT_LABELS } from "translations.constants";
 import BoxHeader from "../BoxHeader";
 
 type JobProfileProps = {};
 
 const JobProfile: React.FC<JobProfileProps> = () => {
   const { colorMode } = useColorMode();
+  const { lang } = useLanguageContext();
+  const pageTrans = ABOUT_LABELS[lang].jobProfile;
+  const jobProfile = pageTrans && isIJobProfile(pageTrans) ? pageTrans : null;
+
   return (
-    <VStack
-      alignItems={{ base: "flex-start" }}
-      border="1px solid"
-      borderColor={colorMode === "dark" ? "whiteAlpha.200" : "blackAlpha.200"}
-      p="8"
-      borderRadius="md"
-    >
-      <BoxHeader title="Profil zawodowy" />
-      <Text>
-        {`Od 2012 roku pracuje jako frontend developer. W trakcie tego okresu
-        byłem zaangażowany w kilkanaście a może i więcej projektów komercyjnych.
-        Moją główną odpowiedzialnością jest tworznie warstwy prezentacyjnej
-        stron internetowych na bazie projektów graficznych. Obecnie jestem
-        skupiony na podnoszeniu moich kwalifikacji z zakresu JavaScript'u i
-        jestem zaintersowany głównie tego typu projektami.`}
-      </Text>
-    </VStack>
+    jobProfile && (
+      <VStack
+        alignItems={{ base: "flex-start" }}
+        border="1px solid"
+        borderColor={colorMode === "dark" ? "whiteAlpha.200" : "blackAlpha.200"}
+        p="8"
+        borderRadius="md"
+      >
+        <BoxHeader title={jobProfile.title} />
+        <Text>{jobProfile.text}</Text>
+      </VStack>
+    )
   );
 };
 

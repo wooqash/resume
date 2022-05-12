982d6634d05d832a88be08bced53fa2d2e2345c8
wooqash <wooqash.webdesign@gmail.com>
2022-05-12 17:01:29 +0200

    feat(components): added translation keys and loop by items to PersonalInfo component

--- components/PersonalInfo/PersonalInfo.tsx
+++ components/PersonalInfo/PersonalInfo.tsx
@@ -1,86 +1,48 @@
 import { Box, List, ListItem, VStack } from "@chakra-ui/react";
+import { useLanguageContext } from "contexts/language";
+import { isIPersonalInfo } from "helpers/type-guards.helpers";
+import { ABOUT_LABELS } from "translations.constants";
 import BoxHeader from "../BoxHeader";
 
 type PersonalInfoProps = {};
 
 const PersonalInfo: React.FC<PersonalInfoProps> = () => {
-  return (
-    <VStack
-      alignItems={{ base: "flex-start" }}
-      // border="1px solid"
-      // borderColor={colorMode === "dark" ? "whiteAlpha.200" : "blackAlpha.200"}
-      py={{ lg: 8 }}
+  const { lang } = useLanguageContext();
+  const pageTrans = ABOUT_LABELS[lang].personalInfo;
+  const personalInfo =
+    pageTrans && isIPersonalInfo(pageTrans) ? pageTrans : null;
 
-      // borderRadius="md"
-    >
-      <BoxHeader title="Personal Info" />
-      <List display="flex" flexWrap="wrap" mt="0">
-        <ListItem flex="0 0 50%" maxW="50%" my="0.5rem" pr="4">
-          <Box as="span" mr="1" textTransform="capitalize" color="teal.200">
-            Imię:
-          </Box>
-          <Box as="span" fontWeight="bold" display="block">
-            Łukasz
-          </Box>
-        </ListItem>
-        <ListItem flex="0 0 50%" maxW="50%" my="0.5rem" pr="4">
-          <Box as="span" mr="1" textTransform="capitalize" color="teal.200">
-            Nazwisko:
-          </Box>
-          <Box as="span" fontWeight="bold" display="block">
-            Sobola
-          </Box>
-        </ListItem>
-        <ListItem flex="0 0 50%" maxW="50%" my="0.5rem" pr="4">
-          <Box as="span" mr="1" textTransform="capitalize" color="teal.200">
-            Data urodzenia:
-          </Box>
-          <Box as="span" fontWeight="bold" display="block">
-            17.01.1984
-          </Box>
-        </ListItem>
-        <ListItem flex="0 0 50%" maxW="50%" my="0.5rem" pr="4">
-          <Box as="span" mr="1" textTransform="capitalize" color="teal.200">
-            Narodowość:
-          </Box>
-          <Box as="span" fontWeight="bold" display="block">
-            Polska
-          </Box>
-        </ListItem>
-        <ListItem flex="0 0 50%" maxW="50%" my="0.5rem" pr="4">
-          <Box as="span" mr="1" textTransform="capitalize" color="teal.200">
-            Telefon:
-          </Box>
-          <Box as="span" fontWeight="bold" display="block">
-            +48 796 776 716
-          </Box>
-        </ListItem>
-        <ListItem flex="0 0 50%" maxW="50%" my="0.5rem" pr="4">
-          <Box as="span" mr="1" textTransform="capitalize" color="teal.200">
-            Email:
-          </Box>
-          <Box as="span" fontWeight="bold" display="block">
-            lukasz.sobola@gmail.com
-          </Box>
-        </ListItem>
-        <ListItem flex="0 0 50%" maxW="50%" my="0.5rem" pr="4">
-          <Box as="span" mr="1" textTransform="capitalize" color="teal.200">
-            Miejsce zamieszkania:
-          </Box>
-          <Box as="span" fontWeight="bold" display="block">
-            Łódź 91-165, ul. Prof. Eugeniusza Romera 21
-          </Box>
-        </ListItem>
-        <ListItem flex="0 0 50%" maxW="50%" my="0.5rem" pr="4">
-          <Box as="span" mr="1" textTransform="capitalize" color="teal.200">
-            Języki:
-          </Box>
-          <Box as="span" fontWeight="bold" display="block">
-            Polski (natywny), Angielski (B1)
-          </Box>
-        </ListItem>
-      </List>
-    </VStack>
+  return (
+    personalInfo && (
+      <VStack alignItems={{ base: "flex-start" }} py={{ lg: 8 }}>
+        <BoxHeader title={personalInfo.title} />
+        <List display="flex" flexWrap="wrap" mt="0" w="100%">
+          {personalInfo.info.map((item) => {
+            return (
+              <ListItem
+                flex="0 0 50%"
+                maxW="50%"
+                my="0.5rem"
+                pr="4"
+                key={item.id}
+              >
+                <Box
+                  as="span"
+                  mr="1"
+                  textTransform="capitalize"
+                  color="teal.200"
+                >
+                  {`${item.label}:`}
+                </Box>
+                <Box as="span" fontWeight="bold" display="block">
+                  {item.text}
+                </Box>
+              </ListItem>
+            );
+          })}
+        </List>
+      </VStack>
+    )
   );
 };
 

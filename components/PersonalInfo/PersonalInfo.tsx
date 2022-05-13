6e48177698ca0d10b43d0b562f3db27e36821130
wooqash <wooqash.webdesign@gmail.com>
2022-05-13 11:24:43 +0200

    feat(components): added icons to PersonalInfo list items

--- components/PersonalInfo/PersonalInfo.tsx
+++ components/PersonalInfo/PersonalInfo.tsx
@@ -1,16 +1,59 @@
-import { Box, List, ListItem, VStack } from "@chakra-ui/react";
+import { PersonalInfoType } from "@/types/personal-info.interface";
+import {
+  Box,
+  Icon,
+  List,
+  ListItem,
+  useColorMode,
+  VStack,
+} from "@chakra-ui/react";
 import { useLanguageContext } from "contexts/language";
 import { isIPersonalInfo } from "helpers/type-guards.helpers";
+import {
+  FiCalendar,
+  FiFlag,
+  FiGlobe,
+  FiMail,
+  FiMapPin,
+  FiPhone,
+  FiUser,
+  FiUserPlus,
+} from "react-icons/fi";
 import { ABOUT_LABELS } from "translations.constants";
 import BoxHeader from "../BoxHeader";
 
 type PersonalInfoProps = {};
 
 const PersonalInfo: React.FC<PersonalInfoProps> = () => {
+  const { colorMode } = useColorMode();
   const { lang } = useLanguageContext();
   const pageTrans = ABOUT_LABELS[lang].personalInfo;
   const personalInfo =
     pageTrans && isIPersonalInfo(pageTrans) ? pageTrans : null;
+
+  const getIcon = (type: PersonalInfoType) => {
+    switch (type) {
+      case "name":
+        return <Icon as={FiUser} mr="1" />;
+      case "surname":
+        return <Icon as={FiUserPlus} mr="1" />;
+      case "dateofbirth":
+        return <Icon as={FiCalendar} mr="1" />;
+      case "nationality":
+        return <Icon as={FiGlobe} mr="1" />;
+      case "phoneno":
+        return <Icon as={FiPhone} mr="1" />;
+      case "email":
+        return <Icon as={FiMail} mr="1" />;
+      case "address":
+        return <Icon as={FiMapPin} mr="1" />;
+      case "langs":
+        return <Icon as={FiFlag} mr="1" />;
+      default:
+        let x: never = type;
+        return;
+    }
+  };
 
   return (
     personalInfo && (
@@ -30,8 +73,11 @@ const PersonalInfo: React.FC<PersonalInfoProps> = () => {
                   as="span"
                   mr="1"
                   textTransform="capitalize"
-                  color="teal.200"
+                  color={colorMode === "dark" ? "teal.200" : "teal.700"}
+                  display="flex"
+                  alignItems="center"
                 >
+                  {getIcon(item.type)}
                   {`${item.label}:`}
                 </Box>
                 <Box as="span" fontWeight="bold" display="block">

dfef8255ec9a399877453dd1b9a558d4e5bd6d4c
wooqash <wooqash.webdesign@gmail.com>
2022-05-13 11:35:10 +0200

    feat(types): added PersonalInfoType and extended interface IPersonalInfoItem by type

--- types/personal-info.interface.ts
+++ types/personal-info.interface.ts
@@ -1,5 +1,16 @@
+export type PersonalInfoType =
+  | "name"
+  | "surname"
+  | "dateofbirth"
+  | "nationality"
+  | "phoneno"
+  | "email"
+  | "address"
+  | "langs";
+
 export interface IPersonalInfoItem {
   id: string | number;
+  type: PersonalInfoType;
   label: string;
   text: string;
 }

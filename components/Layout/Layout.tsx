b8c3a7ad89629be2648c809476bd1cf860b6cd9d
wooqash <wooqash.webdesign@gmail.com>
2022-05-25 05:38:27 +0200

    feat(components): added flex element to main layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,6 +1,5 @@
+import { Flex } from "@chakra-ui/react";
 import { ReactChild } from "react";
-import LangSwitcher from "../LangSwitcher";
-import ModeSwitcher from "../ModeSwitcher";
 import Navbar from "../Navbar";
 
 type LayoutProps = {
@@ -11,7 +10,19 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
       <Navbar />
-      <main>{children}</main>
+      <main>
+        <Flex
+          py={{ base: 20, lg: 12 }}
+          pl={{ base: 4 }}
+          pr={{ base: 4, lg: 28, "2xl": 4 }}
+          alignItems={{ base: "flex-start", sm: "center" }}
+          maxW="container.xl"
+          m="0 auto"
+          direction="column"
+        >
+          {children}
+        </Flex>
+      </main>
     </>
   );
 };

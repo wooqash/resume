0b34ee2845d93076f2775eb6471c57c9e92074e7
wooqash <wooqash.webdesign@gmail.com>
2022-06-07 06:25:23 +0200

    feat(components): added content and styles to Projects componenets

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import Image from "next/image";
-import { IProject } from "@/types/portfolio.interface";
 import { isIPortfolio } from "helpers/type-guards.helpers";
 import {
   Box,
+  Heading,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -17,6 +17,7 @@ import {
 import { PORTFOLIO_LABELS } from "translations.constants";
 import { useLanguageContext } from "contexts/language";
 import ProjectDetailsList from "./ProjectDetailsList";
+import SwiperCarousel from "../SwiperCarousel";
 
 type ProjectsProps = {};
 
@@ -37,6 +38,19 @@ const Projects: React.FC<ProjectsProps> = () => {
     (project) => project.id === projectId
   );
 
+  const selectedProjectImages = selectedProject?.details?.images?.map(
+    (image) => (
+      <Image
+        // loader={myLoader}
+        src={image.url}
+        // alt="Picture of the author"
+        width={image.width}
+        height={image.height}
+        key={image.id}
+      />
+    )
+  );
+
   const openProjectDetails = (id: string | number) => {
     setProjectId(id);
     onOpen();
@@ -45,26 +59,67 @@ const Projects: React.FC<ProjectsProps> = () => {
   return (
     <>
       {portfolio?.projects && (
-        <SimpleGrid
-          minChildWidth="250px"
-          spacing="40px"
-          w="100%"
-          textAlign="center"
-        >
+        <SimpleGrid minChildWidth="250px" spacing="40px" w="100%">
           {portfolio?.projects.map((project) => {
             return (
               <Box
                 as="a"
-                onClick={() => openProjectDetails(project.id)}
+                borderRadius="5px"
+                cursor="pointer"
                 key={project.id}
+                minH="250px"
+                onClick={() => openProjectDetails(project.id)}
+                overflow="hidden"
+                position="relative"
+                _before={{
+                  background: "teal.600",
+                  content: `""`,
+                  height: "100%",
+                  left: 0,
+                  opacity: 0,
+                  position: "absolute",
+                  top: 0,
+                  transition: ".5s",
+                  visibility: "hidden",
+                  width: "100%",
+                  zIndex: 1,
+                }}
+                _hover={{
+                  _before: { visibility: "visible", opacity: 1 },
+                  span: {
+                    opacity: 1,
+                    transform: "translateY(0)",
+                    visibility: "visible",
+                  },
+                }}
               >
                 <Image
                   // loader={myLoader}
-                  src="https://picsum.photos/350/250?random=1"
+                  src={project.mainImage.url}
                   alt="Picture of the author"
-                  width={350}
-                  height={250}
+                  // width={350}
+                  // height={250}
+                  layout="fill"
                 />
+                <Box
+                  as="span"
+                  fontFamily="heading"
+                  left="0"
+                  letterSpacing="2px"
+                  margin="0"
+                  opacity="0"
+                  position="absolute"
+                  textAlign="center"
+                  textTransform="uppercase"
+                  top="48%"
+                  transform="translateY(-50%)"
+                  transition=".5s"
+                  visibility="hidden"
+                  w="100%"
+                  zIndex="5"
+                >
+                  {project.title}
+                </Box>
               </Box>
             );
           })}
@@ -74,7 +129,11 @@ const Projects: React.FC<ProjectsProps> = () => {
         <Modal isOpen={isOpen} onClose={onClose} size="2xl">
           <ModalOverlay />
           <ModalContent>
-            <ModalHeader>{selectedProject.title}</ModalHeader>
+            <ModalHeader textAlign="center">
+              <Heading as="h3" size="xl" variant="brand">
+                {selectedProject.title}
+              </Heading>
+            </ModalHeader>
             <ModalCloseButton />
             <ModalBody>
               {portfolio && (
@@ -83,14 +142,10 @@ const Projects: React.FC<ProjectsProps> = () => {
                   details={selectedProject.details}
                 />
               )}
+              {selectedProjectImages && (
+                <SwiperCarousel>{selectedProjectImages}</SwiperCarousel>
+              )}
             </ModalBody>
-
-            <ModalFooter>
-              {/* <Button colorScheme='blue' mr={3} onClick={onClose}>
-            Close
-          </Button>
-          <Button variant='ghost'>Secondary Action</Button> */}
-            </ModalFooter>
           </ModalContent>
         </Modal>
       )}

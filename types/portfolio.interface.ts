33492f1fc536ff3ae193769ec0ec952a0303b7c5
wooqash <wooqash.webdesign@gmail.com>
2022-06-11 06:11:40 +0200

    feat(types): added IRecommendation interface

--- types/portfolio.interface.ts
+++ types/portfolio.interface.ts
@@ -4,6 +4,7 @@ import { ISimpleImage } from "./media.interface";
 export interface IProject {
   id: string | number;
   title: string;
+  mainImage: ISimpleImage; //TODO: need to swap to IMedia after connect with StrapiCMS
   details: {
     type: string;
     technologies: string[];
@@ -11,16 +12,28 @@ export interface IProject {
     url: ILink;
     images?: ISimpleImage[]; //TODO: need to swap to IMedia after connect with StrapiCMS
   };
-  mainImage: ISimpleImage; //TODO: need to swap to IMedia after connect with StrapiCMS
+}
+
+export interface IReference {
+  id: string | number;
+  client: string;
+  text: string;
+  image?: ISimpleImage; //TODO: need to swap to IMedia after connect with StrapiCMS
+}
+
+export interface IRecommendation {
+  title?: string;
+  references: IReference[];
 }
 
 export interface IPortfolio {
   title?: string;
-  productDetailsLabels: {
+  projectDetailsLabels: {
     type: string;
     technologies: string;
     client: string;
     url: string;
   };
   projects: IProject[];
+  recommendations: IRecommendation;
 }

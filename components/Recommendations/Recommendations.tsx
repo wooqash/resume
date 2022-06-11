3032f328738267a78d55404d78168e16e39af002
wooqash <wooqash.webdesign@gmail.com>
2022-06-11 06:03:18 +0200

    feat(components): added structure to Recommendation component

--- components/Recommendations/Recommendations.tsx
+++ components/Recommendations/Recommendations.tsx
@@ -1,7 +1,50 @@
-type RecommendationsProps = {};
+import { IRecommendation } from "@/types/portfolio.interface";
+import { Avatar, Box, Flex, Text, VStack } from "@chakra-ui/react";
+import BoxHeader from "../BoxHeader";
+import SwiperCarousel from "../SwiperCarousel";
 
-const Recommendations: React.FC<RecommendationsProps> = () => {
-  return <div>Recommendations</div>;
+type RecommendationsProps = {
+  recommendations: IRecommendation;
+};
+
+const Recommendations: React.FC<RecommendationsProps> = ({
+  recommendations,
+}) => {
+  const { title, references } = recommendations;
+
+  const referencesToShow = references.map((reference) => (
+    <VStack textAlign="center" mb="4" key={reference.id}>
+      {reference.image && (
+        <Avatar
+          src={reference.image.url}
+          name=""
+          size="lg"
+          overflow="hidden"
+          my="4"
+        ></Avatar>
+      )}
+      <Text mb="6">{reference.text}</Text>
+      <Text>{reference.client}</Text>
+    </VStack>
+  ));
+  return (
+    <Flex
+      w="100%"
+      justifyContent="center"
+      alignItems="center"
+      my="16"
+      flexDirection="column"
+    >
+      {title && <BoxHeader title={title} />}
+      {references && (
+        <Flex w="80%" justifyContent="center">
+          <SwiperCarousel nav={false} autoplay={true}>
+            {referencesToShow}
+          </SwiperCarousel>
+        </Flex>
+      )}
+    </Flex>
+  );
 };
 
 export default Recommendations;
